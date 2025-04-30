fdeb10e8b3dcddf27ed049e523db50c53744847e
Emre DURGUNLU <emredurgunlu@gmail.com>
2025-04-30 16:01:27 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner";
+import { ModeToggle } from "@/components/theme-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,9 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <div style={{ position: "fixed", top: 16, right: 16, zIndex: 50 }}>
+            <ModeToggle />
+          </div>
           {children}
         </ThemeProvider>
         <Toaster />
